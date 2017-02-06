9967a515a706dc5dae53d071a6de77b36130a122
nadusumilli <nadusumilli@unomaha.edu>
2017-02-05 20:41:39 -0600

    Background color added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,19 +23,23 @@
     {{Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css')}}
     {{Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css')}}
     {{Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js')}}
-    <style type="text/css">
-        body {
-                background: url("https://www.rolandberger.com/img/Background-Folder/Backgrounds/financial_services_quote_background_new_background_stage_tablet.jpg") no-repeat center center fixed;
-                -webkit-background-size: cover;
-                -moz-background-size: cover;
-                -o-background-size: cover;
-                background-size: cover;
-        }
-    </style>
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        /* body {
+            background-color: yellow;
+        } */
+         body{
+            background: url('images/background.jpg') no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+        } 
+    }
+    </style>
         
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
